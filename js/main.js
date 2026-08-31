0eab15f09bab32077862a71df478e18edb5b0ac0
luskisaulo <luskixsaulo@Gmail.com>
2026-08-30 23:09:43 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ const loadingManager = new THREE.LoadingManager();
 const IMAGES_FOLDER = './assets/sprites/imagens/';
 const PAINTING_IMAGE_URL = IMAGES_FOLDER + 'sofiagatinha.jpeg';
 const MEMORY_IMAGE_URLS = [
-  IMAGES_FOLDER + 'memoria1.jpg', // quadro 1 do corredor (esquerda)
+  IMAGES_FOLDER + 'memoria1.jpeg', // quadro 1 do corredor (esquerda)
   IMAGES_FOLDER + 'memoria2.jpg', // quadro 2 do corredor (direita)
   IMAGES_FOLDER + 'memoria3.jpg', // quadro 3 do corredor (esquerda)
 ];
